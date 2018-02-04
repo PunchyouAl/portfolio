7b0ec05cf61aa5aef7f7ecc9370f3bd942e62188
PunchyouAl <alex.williamson85@gmail.com>
2018-02-04 23:12:48 +0000

    Actual Site!

---
+++ Portfolio2017/assets/js/preloader.js
@@ -0,0 +1,26 @@
+ $(document).ready(function () {          
+
+            setTimeout(function() {
+                $('.pre').slideUp("slow");
+            }, 3000);
+
+            setTimeout(function() {
+                $('.pre1').slideUp("slow");
+            }, 3250);
+
+            setTimeout(function() {
+                $('.pre2').slideUp("slow");
+            }, 3500);
+
+            setTimeout(function() {
+                $('.pre3').slideUp("slow");
+            }, 3400);
+
+            setTimeout(function() {
+                $('.pre4').slideUp("slow");
+            }, 3750);                    
+
+            setTimeout(function() {
+                $('.pre .loader').fadeOut("slow");
+            }, 3000);
+});
