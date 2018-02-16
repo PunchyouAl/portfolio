31c4387c63c95c1dcbf162fa02cad8b103828dd4
Alex W <alex.williamson85@gmail.com>
2018-02-16 18:22:12 +0000

    Scrollmagic Addition

---
+++ Portfolio2017/assets/js/scrollmagic.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    //TURN BLOCKS RED//
+
+    /*
+
+    $(function() {
+        var controller = new ScrollMagic.Controller();
+        var blockTween = new TweenMax.to('.about-line', 1.5, {
+            backgroundColor: 'red'
+        });
+        var containerScene = new ScrollMagic.Scene({
+                triggerElement: '#one'
+            })
+            .setTween(blockTween)
+            .addIndicators()
+            .addTo(controller);
+    });
+
+	*/
+
+	 //Pin Blocks//
+
+	 /*
+
+    $(function() {
+        var pin = new ScrollMagic.Controller();
+        var blockScene = new ScrollMagic.Scene({
+            triggerElement: '#one'
+        })
+        .setPin('#one')
+        .addIndicators()
+        .addTo(pin);
+    });
+
+    */
+});
