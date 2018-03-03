8cc8c6d09fb83ef67deedc1a2da8fad8a32b8438
PunchyouAl <alex.williamson85@gmail.com>
2018-03-03 21:47:19 +0000

    tilt positioning

--- Portfolio2017/assets/js/preloader.js
+++ Portfolio2017/assets/js/preloader.js
@@ -1,26 +1,28 @@
+
+
  $(document).ready(function () {          
 
             setTimeout(function() {
                 $('.pre').slideUp("slow");
-            }, 3000);
+            }, /*3000*/1);
 
             setTimeout(function() {
                 $('.pre1').slideUp("slow");
-            }, 3250);
+            }, /*3250*/1);
 
             setTimeout(function() {
                 $('.pre2').slideUp("slow");
-            }, 3500);
+            }, /*3500*/1);
 
             setTimeout(function() {
                 $('.pre3').slideUp("slow");
-            }, 3400);
+            }, /*3400*/1);
 
             setTimeout(function() {
                 $('.pre4').slideUp("slow");
-            }, 3750);                    
+            }, /*3750*/1);                    
 
             setTimeout(function() {
                 $('.pre .loader').fadeOut("slow");
-            }, 3000);
+            }, /*3000*/1);
 });
--- Portfolio2017/assets/js/scrollmagic.js
+++ Portfolio2017/assets/js/scrollmagic.js
@@ -22,22 +22,15 @@ $(function() {
 
 });
 
-
-
-
-
-
-
-
-
+// TILTY 1 //
 
 $(function() {
 
-    var controllerPortfolioTilt = new ScrollMagic.Controller();
+    var controllerPortfolioTilt1 = new ScrollMagic.Controller();
 
-    var swipe = TweenMax.to('.color-swipe-blue', 1, {
+    var swipe = TweenMax.to('.color-swipe-blue', 1.2, {
         opacity: 1,
-        right: -1000
+        right: -2200
     });
 
     var scene1 = new ScrollMagic.Scene({
@@ -45,7 +38,7 @@ $(function() {
             offset: 0
         })
         .setTween(swipe)
-        .addTo(controllerPortfolioTilt);
+        .addTo(controllerPortfolioTilt1);
 
     scene1.addIndicators();
 
@@ -53,7 +46,7 @@ $(function() {
 
 $(function() {
 
-    var controllerPortfolioTiltWhite = new ScrollMagic.Controller();
+    var controllerPortfolioTiltWhite1 = new ScrollMagic.Controller();
 
     var swipeWhite = TweenMax.to('.color-swipe-white', 1, {
         opacity: 1,
@@ -65,7 +58,7 @@ $(function() {
             offset: 0
         })
         .setTween(swipeWhite)
-        .addTo(controllerPortfolioTiltWhite);
+        .addTo(controllerPortfolioTiltWhite1);
 
     scene1.addIndicators();
 
@@ -73,10 +66,62 @@ $(function() {
 
 $(function() {
 
-    var controllerPortfolioAddShadow = new ScrollMagic.Controller();
+    var controllerPortfolioAddShadow1 = new ScrollMagic.Controller();
 
     new ScrollMagic.Scene({ triggerElement: "#tilty-1" })
         .setClassToggle(".c-logo", "c-logo-shadow")
-        .addTo(controllerPortfolioAddShadow);
+        .addTo(controllerPortfolioAddShadow1);
+
+});
+
+// TILTY 2 //
+
+$(function() {
+
+    var controllerPortfolioTilt2 = new ScrollMagic.Controller();
+
+    var swipe = TweenMax.to('.color-swipe-blue2', 1.2, {
+        opacity: 1,
+        right: -3200
+    });
+
+    var scene1 = new ScrollMagic.Scene({
+            triggerElement: '#tilty-2',
+            offset: 0
+        })
+        .setTween(swipe)
+        .addTo(controllerPortfolioTilt2);
+
+    scene1.addIndicators();
+
+});
+
+$(function() {
+
+    var controllerPortfolioTiltWhite2 = new ScrollMagic.Controller();
+
+    var swipeWhite = TweenMax.to('.color-swipe-white2', 1, {
+        opacity: 1,
+        right: -1000
+    });
+
+    var scene1 = new ScrollMagic.Scene({
+            triggerElement: '#tilty-2',
+            offset: 0
+        })
+        .setTween(swipeWhite)
+        .addTo(controllerPortfolioTiltWhite2);
+
+    scene1.addIndicators();
+
+});
+
+$(function() {
+
+    var controllerPortfolioAddShadow2 = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({ triggerElement: "#tilty-2" })
+        .setClassToggle(".c-logo", "c-logo-shadow")
+        .addTo(controllerPortfolioAddShadow2);
 
 });
